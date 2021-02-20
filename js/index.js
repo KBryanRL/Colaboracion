8b98f7eaca0350e51207d246f344f75c2abf7b9e
KBryanRL <KBryanRL.bryan_norck@outlook.com>
2021-02-20 03:06:10 -0600

    cambios cards y nav

--- js/index.js
+++ js/index.js
@@ -6,9 +6,16 @@ owl.owlCarousel({
     autoplayTimeout: 4000,
     autoplayHoverPause: true
 });
-$('.play').on('click', function() {
-    owl.trigger('play.owl.autoplay', [1000])
-})
-$('.stop').on('click', function() {
-    owl.trigger('stop.owl.autoplay')
-})
+
+var owl1 = $('.owl-carousel');
+owl1.owlCarousel({
+    center: true,
+    loop: true,
+    items: 4,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    margin: false,
+    nav: true,
+});
