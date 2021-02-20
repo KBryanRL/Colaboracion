b80ace493723952ff0131f931c772a0370196890
KBryanRL <KBryanRL.bryan_norck@outlook.com>
2021-02-19 21:29:31 -0600

    carousell con owl.carousell

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function() {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function() {
+    owl.trigger('stop.owl.autoplay')
+})
